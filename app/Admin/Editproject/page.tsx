1557cc6cd95475472b413096187cfa6242f0f744
Kitsada <66022387@up.ac.th>
2025-09-28 22:09:02 +0700

    Build6

---
+++ app/Admin/Editproject/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function EditProjectPage() {
+  return (
+    <div>
+      <h1>Edit Project</h1>
+      {/* เพิ่มเนื้อหาของคุณที่นี่ */}
+    </div>
+  );
+}
